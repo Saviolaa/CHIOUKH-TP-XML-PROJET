7e925e77d45c2996c355fdc47291b03aeeefb104
Saviolaa <polos30@hotmail.fr>
2014-05-08 15:55:31 +0200

    Maintenant ça ressemble a un CV. Fin structuration CV. Fin, PUT GET DELETE

--- src/main/java/Rest/controller/cvController.java
+++ src/main/java/Rest/controller/cvController.java
@@ -28,23 +28,13 @@ public class cvController {
         return moncvList;
     }
 
-
-    //const vide
     public cvController() {
     }
 
-/*
-@RequestMapping(value="{id}", method = RequestMethod.GET)
-public @ResponseBody
-cv getResumeInXML(@PathVariable String id){
-        cv resumes = new cv("CHIOUKH", "Lyes", "Objectifs");
-        return resumes;
-    }
-*/
 
-    //Ajout de cvs
     @RequestMapping(value = "/put", method = RequestMethod.PUT)
     public ResponseEntity<cvList>  putCvInList(@RequestBody cv cv) {
+
         moncvList.AjouterCV(cv);
         ResponseEntity<cvList> cvListResponseEntity=new ResponseEntity<cvList>(HttpStatus.OK);
         return cvListResponseEntity;
@@ -61,55 +51,16 @@ cv getResumeInXML(@PathVariable String id){
     //Affichage de tout les CVS
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<cvList> getLesCvsInXML() {
-        //pour tester ***************************************
-        ArrayList<String> scolarites = new ArrayList<String>();
-        ArrayList<String> experiencePro = new ArrayList<String>();
-        ArrayList<String> langues = new ArrayList<String>();
-        scolarites.add("Licence Informatique, univ rouen, 2011");
-        scolarites.add("Master Informatique, univ rouen, 2013");
-        experiencePro.add("Expert test a google, 4 ans");
-        experiencePro.add("Developpeur a Fcabook, 6mois");
-        langues.add("Français");
-        langues.add("Arabe");
-        moncvList.AjouterCV(new cv(1, "chioukh", "lyes", "Recherche Stage", scolarites,
-                experiencePro,
-                langues));
 
-        ArrayList<String> scolarites1 = new ArrayList<String>();
-        ArrayList<String> experiencePro1 = new ArrayList<String>();
-        ArrayList<String> langues1 = new ArrayList<String>();
-        scolarites1.add("Licence media, univ rouen, 2011");
-        scolarites1.add("Master chronique, univ rouen, 2013");
-        experiencePro1.add("Chroniqueur NRJ, 4 ans");
-        experiencePro1.add("Canal+ , 6mois");
-        langues1.add("Français");
-        langues1.add("Anglais");
-        moncvList.AjouterCV(new cv(2, "Nicolas", "Bideau", "Recherche Stage", scolarites1,
-                experiencePro1,
-                langues1));
-        //Fin pour tester ***************************************
         ResponseEntity<cvList> cvListResponseEntity=new ResponseEntity<cvList>(moncvList,HttpStatus.OK);
         return cvListResponseEntity;
     }
 
     //Suppression d'un seul cv, par num
-    @RequestMapping(value="/{numero}", method = RequestMethod.DELETE)
+    @RequestMapping(value="/delete/{numero}", method = RequestMethod.DELETE)
     public ResponseEntity<cv> deleteCvInXML(@PathVariable int numero) {
         moncvList.SupprimerCV(numero);
         ResponseEntity<cv> cvDeleteResponseEntity=new ResponseEntity<cv>(HttpStatus.OK);
         return cvDeleteResponseEntity;
     }
-
-    public static void main(String args[]) throws Exception {
-
-        cvController mes_cvs = new cvController();
-        System.out.println("Ajout d'un CV");
-        mes_cvs.putCvInList(new cv(1, "chioukh", "lyes", "Objectif", new ArrayList<String>(),
-                new ArrayList<String>(),
-                new ArrayList<String>()));
-/*
-
-        mes_cvs.getCvInXML(1);
-        */
-}
 }
--- src/main/java/Rest/model/cv.java
+++ src/main/java/Rest/model/cv.java
@@ -1,88 +1,104 @@
 package Rest.model;
 
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by LYES-PC on 08/04/14.
  */
 @XmlRootElement(name = "cv")
-@XmlType(propOrder = {//"numero",
-"name", "prenom", "objectif", "scolarites", "experiencePro", "langues" })
+@XmlType(propOrder = {"numero","name", "prenom", "objectif", "scolarites", "listexperience", "langues" })
 public class cv {
 
-    /*Numéro de CV*/
-   // int numero;
-    /*Nom*/
-    String name;
-    /*Prenom*/
-    String prenom;
-    /*Objectifs*/
-    String objectif;
-    /*Parcours Scolaire*/
-    @XmlElement
-    List<String> scolarites;
-    /*Exépriences*/
-    @XmlElement
-    List<String> experiencePro;
-    /*Langues maitrisées*/
-    @XmlElement
-    List<String> langues;
 
+    /*Numéro de CV*/
+
+    private int numero;
+    /*Nom*/
+
+    private String name;
+    /*Prenom*/
+
+    private String prenom;
+    /*Objectifs*/
+
+    private String objectif;
+    /*Parcours Scolaire*/
+
+    private ListeScolarites scolarites;
+    /*Exépriences*/
+    private ListeExperiences listexperience;
+
+    /*Langues maitrisées*/
+    private ListeLangues langues;
+
+    public cv(int numero,String name, String prenom, String objectif, ListeScolarites scolarites, ListeExperiences experiences, ListeLangues langues) {
+        this.numero = numero;
+        this.name = name;
+        this.prenom = prenom;
+        this.objectif = objectif;
+        this.scolarites = scolarites;
+        this.listexperience = experiences;
+        this.langues = langues;
+    }
 
     public cv() {
-
     }
 
-    public String getName (){
-        return name;
-    }
-
-    @XmlElement
-    public void setName (String name){
-        this.name = name;
-    }
-/*
-    public int getNumero (){
+    public int getNumero() {
         return numero;
     }
 
-    @XmlElement
-    public void setNumero (int numero){
+    public void setNumero(int numero) {
         this.numero = numero;
     }
-*/
-    public String getPrenom (){
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrenom() {
         return prenom;
     }
 
-    @XmlElement
-    public void setPrenom (String prenom){
+    public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
 
-    public String getObjectif (){
+    public String getObjectif() {
         return objectif;
     }
 
-    @XmlElement
-    public void setObjectif (String objectif){
+    public void setObjectif(String objectif) {
         this.objectif = objectif;
     }
 
-    public cv(int numero, String name, String prenom, String objectif,
-              List<String> scolarites,
-              List<String> experiencePro,
-              List<String> langues ){
-     //   this.numero=numero;
-        this.name=name;
-        this.prenom=prenom;
-        this.objectif=objectif;
+    public ListeScolarites getScolarites() {
+        return scolarites;
+    }
+
+    public void setScolarites(ListeScolarites scolarites) {
         this.scolarites = scolarites;
-        this.experiencePro = experiencePro;
+    }
+
+    public ListeExperiences getListexperience() {
+        return listexperience;
+    }
+
+    public void setListexperience(ListeExperiences listexperience) {
+        this.listexperience = listexperience;
+    }
+
+    public ListeLangues getLangues() {
+        return langues;
+    }
+
+    public void setLangues(ListeLangues langues) {
         this.langues = langues;
     }
 }
